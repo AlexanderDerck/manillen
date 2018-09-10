d1b332ee69b67406db7049836dbd0b3e9d72b30e
alexander <alexander.derck@gmail.com>
2018-09-10 22:00:58 +0200

    Add sass variables for page-width

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { AppBar, Typography, Toolbar } from '@material-ui/core';
 
+import styles from './Header.module.scss';
+
 const Header = () => (
   <AppBar position='static'>
     <Toolbar>
-      <Typography variant="title" color="inherit">
-        Manillen
-      </Typography>
+      <div className={styles.container}>
+        <Typography variant="title" color="inherit">
+          Manillen
+        </Typography>
+      </div>
     </Toolbar>
   </AppBar>
 );
