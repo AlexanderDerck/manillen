c9fc42881bc1ceabdac100b34a6a1d73eacf671e
alexander <alexander.derck@gmail.com>
2018-09-11 21:21:14 +0200

    Fix proptypes for Hand, add required key property

--- src/components/Hand/Hand.jsx
+++ src/components/Hand/Hand.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card } from '../Card';
 
 import styles from './Hand.module.scss';
 
 const Hand = props => {
  
   const flexBasis = 100 / props.handSize;   // Flex basis = 100% / initial amount of cards in this hand 
-  const children = props.children || [];    // Not required to pass any cards at all!
+  const children = [].concat(props.children);    // Not required to pass any cards at all!
   const percentToOverlap = 50;              // The percentage adjucant cards will overlap
   /* Align all cards towards center by moving them all right based on current 
    * amount of children - 1 because first child is not translated! */
@@ -19,7 +18,7 @@ const Hand = props => {
     const translateX = translateX_Overlap + translateX_AlignCenter;    
 
     return (
-      <div className={styles.item} style={{ flexBasis: `${flexBasis}%`, transform: `translate3d(${translateX}%, 0, 0)` }}>
+      <div key={index} className={styles.item} style={{ flexBasis: `${flexBasis}%`, transform: `translate3d(${translateX}%, 0, 0)` }}>
         <div className={styles.transitionHover}>
           {card}
         </div>     
@@ -35,12 +34,11 @@ const Hand = props => {
 };
 
 Hand.propTypes = {
-  children: PropTypes
-    .oneOfType([
-      PropTypes.arrayOf(PropTypes.instanceOf(Card)),
-      PropTypes.instanceOf(Card)
-    ]),
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.element),
+    PropTypes.element
+  ]),
   handSize: PropTypes.number.isRequired
-}
+};
 
 export { Hand };
