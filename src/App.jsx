8669b079c352dc90d70380fb361c41c571ce76e0
alexander <alexander.derck@gmail.com>
2018-09-11 23:17:26 +0200

    Add test decks to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,36 @@ import React, { Component } from 'react';
 import { MuiThemeProvider } from '@material-ui/core';
 import { Header } from './components/Header';
 import muiTheme from './muiTheme';
-import { Card } from './components/Card/Card';
+import { Hand } from './components/Hand';
+import { Card, CardRank, CardSuit } from './components/Card';
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={muiTheme}>
         <Header />
-        <div style={{width: 200, height: 100}}>
-          <Card />
-        </div>       
+        <div style={{width: 1000, margin: 20, border: '3px solid red'}}>
+          <Hand handSize={8}>
+            <Card rank={CardRank.Seven} suit={CardSuit.Hearts} />
+            <Card rank={CardRank.Eight} suit={CardSuit.Hearts} />
+            <Card rank={CardRank.Nine} suit={CardSuit.Hearts} />
+            <Card rank={CardRank.Ten} suit={CardSuit.Hearts} />
+            <Card rank={CardRank.Jack} suit={CardSuit.Hearts} />
+            <Card rank={CardRank.Queen} suit={CardSuit.Hearts} />
+            <Card rank={CardRank.King} suit={CardSuit.Hearts} />
+            <Card rank={CardRank.Ace} suit={CardSuit.Hearts} />
+          </Hand>
+        </div>     
+        <div style={{width: 1000, margin: 20, border: '3px solid red'}}>
+          <Hand handSize={8}>
+            <Card rank={CardRank.Seven} suit={CardSuit.Hearts} />
+            <Card rank={CardRank.Eight} suit={CardSuit.Hearts} />
+            <Card rank={CardRank.Nine} suit={CardSuit.Hearts} />
+            <Card rank={CardRank.Ten} suit={CardSuit.Hearts} />
+            <Card rank={CardRank.Jack} suit={CardSuit.Hearts} />
+            <Card rank={CardRank.Queen} suit={CardSuit.Hearts} />
+          </Hand>
+        </div>      
       </MuiThemeProvider>
     );
   }
