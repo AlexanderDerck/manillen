1a709b6a9e95db833992a572f859e970ee6302a7
alexander <aderck@gmail.com>
2018-09-19 00:01:48 +0200

    Add horizontal prop to CardBack component

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 import { MuiThemeProvider } from '@material-ui/core';
 import { Header } from './components/Header';
 import muiTheme from './muiTheme';
-import { Hand, OpponentHand } from './components/Hand';
+import { CardBack } from './components/Card';
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={muiTheme}>
         <Header />  
-        <div style={{width: 1000, margin: 20, border: '3px solid red'}}>
-          <Hand maxAmountOfCards={8} />
-        </div> 
-        <div style={{width: 1000, margin: 20, border: '3px solid red'}}>
-          <OpponentHand amountOfCards={8} maxAmountOfCards={8} />
+        <div style={{width: 200, margin: 20, border: '3px solid red'}}>
+          <CardBack />
+          <CardBack horizontal />
         </div> 
       </MuiThemeProvider>
     );
--- src/components/Card/svg/CardBackSvgUse.jsx
+++ src/components/Card/svg/CardBackSvgUse.jsx
@@ -3,18 +3,20 @@ import PropTypes from 'prop-types';
 
 import cardSvg from 'assets/svg-cards.svg';
 
-const CardBackSvgUse = ({ x, y }) => (
-  <use x={x} y={y} xlinkHref={`${cardSvg}#back`} fill="#e81010" />
+const CardBackSvgUse = ({ x, y, rotate }) => (
+  <use x={x} y={y} xlinkHref={`${cardSvg}#back`} fill="#e81010" transform={`rotate(${rotate})`}/>
 );
 
 CardBackSvgUse.propTypes = {
   x: PropTypes.number,
-  y: PropTypes.number
+  y: PropTypes.number,
+  rotate: PropTypes.number
 };
 
 CardBackSvgUse.defaultProps = {
   x: 0,
-  y: 0
+  y: 0,
+  rotate: 0
 };
 
 export { CardBackSvgUse };
