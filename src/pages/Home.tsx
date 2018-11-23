9692dfed97848d0461d5ae314b57103c23b714c0
alexander <aderck@gmail.com>
2018-11-24 00:45:01 +0100

    Add enum for routes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Routes} from '../constants';
 
 export const Home = () => (
   <div>
-    <Link to="/login">Login</Link>
-    <Link to="/game">Game</Link>
+    <Link to={Routes.Login}>Login</Link>
+    <Link to={Routes.Game}>Game</Link>
   </div>
 );
