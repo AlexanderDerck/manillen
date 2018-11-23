84aa70149845872df8d1e679ae4a0d6d0e44020f
alexander <aderck@gmail.com>
2018-11-24 00:47:54 +0100

    Make header title nav to home

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { AppBar, Typography, Toolbar } from '@material-ui/core';
-
+import { Link } from 'react-router-dom';
+import { Routes } from '../../constants';
 import styles from './Header.module.scss';
 
 const Header = () => (
   <AppBar position='static'>
     <Toolbar>
       <div className={styles.container}>
-        <Typography variant="title" color="inherit">
-          Manillen
-        </Typography>
+        <Link to={Routes.Home}>
+          <Typography variant="title" color="inherit">
+            Manillen
+          </Typography>
+        </Link>
       </div>
     </Toolbar>
   </AppBar>
