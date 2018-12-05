1aff22068cb3a68ca5572cd2f6bfcb1796e53c45
alexander <aderck@gmail.com>
2018-12-05 22:06:01 +0100

    Add reducer for loginstatus success

--- src/reducers/authentication.ts
+++ src/reducers/authentication.ts
@@ -1,14 +1,31 @@
 import { AuthenticationState } from '../state';
-import { AuthenticationAction } from '../actions/authentication';
+import { AuthenticationAction, GetLoginInfoSuccess } from '../actions/authentication';
+import { ActionTypes } from '../constants';
 
 const initialState: AuthenticationState = {
   isAuthenticated: false,
   user: undefined
 };
 
-export function authentication(state: AuthenticationState = initialState, action: AuthenticationAction) {
+export function authentication(state: AuthenticationState = initialState, action: AuthenticationAction): AuthenticationState {
   switch(action.type) {
-
+    case ActionTypes.AUTHENTICATION_GET_LOGIN_INFO_SUCCESS: return getLoginInfoSuccess(state, action.response);
     default: return state;
   }
+}
+
+function getLoginInfoSuccess(state: AuthenticationState, response: fb.StatusResponse): AuthenticationState {
+  if(response.status !== "connected") {
+    return state;
+  }
+
+  return {
+    isAuthenticated: true,
+    user: {
+      accessToken: response.authResponse.accessToken,
+      firstName: 'Alexander',
+      lastName: 'Derck',
+      userId: response.authResponse.userID
+    }
+  };
 }
