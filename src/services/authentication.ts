8b13598ed839aaa3c898c16e8121255b00fc7c81
AlexanderDerck <aderck@gmail.com>
2018-12-07 12:20:22 +0100

    Refactor the facebook initialization code in a more robust way which takes care of racing conditions

--- src/services/authentication.ts
+++ src/services/authentication.ts
@@ -2,18 +2,37 @@ import { Store } from "redux";
 import { loginSuccess } from "../actions";
 
 interface Window {
+  FB: fb.FacebookStatic,
   fbAsyncInit: () => void
 };
 declare const window: Window;
 
 export function initializeFacebookSdk(store: Store): void {
-  /**
-   * Setting status = true makes sure that the sdk checks for login status and give the 
-   * response to the 'auth.statusChange' event, just as if you'd manually call
-   * FB.getLoginStatus
-   * See: https://developers.facebook.com/docs/reference/javascript/FB.getLoginStatus
-   */
-  window.fbAsyncInit = function() {
+
+  /* Asynchronous flow: if the global 'FB' variable is still undefined,
+     then the facebook script hasn't loaded yet, in that case, provide
+     a global callback that will be called by the facebook code. If the 
+     variable is already present, just call the code right away and forget
+     about the callback. */
+  if(window.FB === undefined) {
+    console.log('FB undefined -> provide callback');
+    window.fbAsyncInit = function() {
+      initialize();
+    };
+  }
+  else {
+    console.log('FB defined -> call init right away');
+    initialize();
+  }
+
+  function initialize() {
+
+    /**
+     * Setting status = true makes sure that the sdk checks for login status and give the 
+     * response to the 'auth.statusChange' event, just as if you'd manually call
+     * FB.getLoginStatus
+     * See: https://developers.facebook.com/docs/reference/javascript/FB.getLoginStatus
+     */
     FB.init({
       appId      : '492331311256888',
       cookie     : true,
@@ -31,5 +50,5 @@ export function initializeFacebookSdk(store: Store): void {
         store.dispatch(loginSuccess(response));        
       }      
     });
-  };
+  }
 }
