a123196402a35b2ecf7f6a0d7258e63565540400
alexander <aderck@gmail.com>
2019-03-12 20:41:25 +0100

    Remove old Game page

--- src/pages/Game.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { GameContainer } from '../containers/GameContainer';
-
-export const Game = () => (
-  <div style={{width: 600, height: 600, margin: 20}}>
-    <GameContainer />
-  </div>
-);
