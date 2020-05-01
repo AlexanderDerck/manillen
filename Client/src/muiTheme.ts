e710475d7043e65570153ab2c9874e779b9db2a4
AlexanderDerck <aderck@gmail.com>
2020-05-01 16:30:26 +0200

    Change some theme colors

--- Client/src/muiTheme.ts
+++ Client/src/muiTheme.ts
@@ -3,9 +3,9 @@ import { createMuiTheme } from '@material-ui/core/styles';
 export const theme = createMuiTheme({
   palette: {
     primary: {
-      light: '#9c786c',
-      main: '#6d4c41',
-      dark: '#40241a'
+      main: '#004d40',
+      light: '#39796b',      
+      dark: '#00251a'
     }
   }
 });
